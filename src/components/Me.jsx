3bf745e215b7b07e003ca7a07600e1585275e773
Dov DEVERS <dov@chavers.org>
2022-02-20 21:04:00 +0000

    update Me.jsx

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -34,11 +34,32 @@ const tools = [
 ]
 
 
+function AboutMe() {
+    return <>
+        <h1 className="text-4xl md:text-6xl font-Lato text-left text-green-500 font-bold ml-3 py-4">About me</h1>
+        <p className="text-xl md:text-3xl break-normal  text-justify font-Lato md:text-left mx-5 leading-9 md:leading-10">
+            I am a student at <a href="https://epita.fr"><img src={epita}
+                                                              className="h-14 w-14 fill-green-500 font-medium mx-auto inline-block"
+                                                              alt="Epita LOGO"/></a> and developer with
+            a passion for learning and
+            problem solving. I am currently working on my master degree in Computer Science.
+            I am a very good eager to learn new technologies and I am always looking for new challenges.
+            I also like photography, you can find few pictures on <a
+            href="https://flic.kr/ps/3YdDB4"
+            className="rounded-full px-1 border-4 font-bold text-xl border-green-500 bg-green-500 hover:bg-green-700 hover:border-green-700 hover:text-white"
+            target="_blank"
+            rel="noreferrer noopener">Flickr <i
+            className="fa-brands fa-flickr "/></a>.
+
+        </p>
+    </>;
+}
+
 export function Me() {
     return (
         <div className=":container bg-black text-white scroll-auto ">
             <div className="h-screen md:flex md:mx-6">
-                <div className="bg-black self-center mx-auto flex-1 w-72 ">
+                <div className="bg-black self-center mx-auto flex-1">
                     <img src={picture} className="h-48 w-48  md:h-80 md:w-80 rounded-full mx-auto" alt="Dov"/>
                     <div className="mt-4">
                         <h1 className="text-2xl font-bold">Renaud-Dov DEVERS</h1>
@@ -56,25 +77,10 @@ export function Me() {
                     <Contact/>
                 </div>
 
-                <div className="md:flex-1 md:flex overflow-hidden px-5 bg-black text-white ">
+                <div className="md:flex-1 md:flex overflow-hidden w-full bg-black text-white ">
                     <div className="md:flex-1 overflow-y-scroll">
-                        <h1  className="text-4xl md:text-6xl font-Lato text-left text-green-500 font-bold py-4">About me</h1>
-                            <p className="text-xl md:text-3xl font-Lato text-left ml-5 leading-9 md:leading-10">
-                            I am a student at <a href="https://epita.fr"><img src={epita}
-                                                                              className="h-14 w-14 fill-green-500 font-medium mx-auto inline-block"
-                                                                              alt="Epita LOGO"/></a> and developer with
-                            a passion for learning and
-                            problem solving. I am currently working on my master degree in Computer Science.
-                            I am a very good eager to learn new technologies and I am always looking for new challenges.
-                            I also like photography, you can find few pictures on <a
-                            href="https://flic.kr/ps/3YdDB4"
-                            className="rounded-full px-1 border-4 font-bold text-xl border-green-500 bg-green-500 hover:bg-green-700 hover:border-green-700 hover:text-white"
-                            target="_blank"
-                            rel="noreferrer noopener">Flickr <i
-                            className="fa-brands fa-flickr "/></a>.
-
-                        </p>
-                        <h1 className="text-4xl md:text-6xl font-Lato text-left text-green-500  font-bold pt-8">Skills</h1>
+                        <AboutMe/>
+                        <h1 className="text-4xl md:text-6xl font-Lato text-left text-green-500 ml-3 font-bold pt-8">Skills</h1>
                         <h1 className="text-xl font-Lato text-left text-green-500 ml-5 font-bold py-8">Languages</h1>
                         <div className="flex flex-wrap gap-4 ml-5">
                             {skills.map((item, index) => (
@@ -95,7 +101,8 @@ export function Me() {
 
                             ))}
                         </div>
-                        <h1 className="text-4xl md:text-6xl font-Lato text-left text-green-500  font-bold py-8">My Projects</h1>
+                        <h1 className="text-4xl md:text-6xl font-Lato text-left text-green-500 ml-3 font-bold py-8">My
+                            Projects</h1>
                         <Projects/>
                         <Footer/>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,36 +9,42 @@ const list = [
         name: 'Opticalloop OCR',
         description: 'Opticalloop is OCR application made in C to recognise and solve sudokus.',
         picture: 'https://opticalloop.bugbear.fr/picture/our_project.gif',
-        link: 'https://opticalloop.bugbear.fr/'
+        link: 'https://opticalloop.bugbear.fr/',
+        tech: ["devicon-c-plain", "devicon-python-plain"]
     },
     {
         name: 'Panic at Tortuga',
         description: 'Panic at Tortuga is a video game developed on Unity Engine during Epita\'s S2 project in a team of 4.',
         picture: panic,
-        link: 'https://lifeinvaders.github.io/website/'
+        link: 'https://lifeinvaders.github.io/website/',
+        tech: ["devicon-csharp-plain", "devicon-unity-original"]
     },
     {
         name: "CheckStudents",
         description: "CheckStudent is Discord bot written in Python to take the attendance in class used in several servers around the world.",
         picture: checkstudents,
         link: "https://github.com/Renaud-Dov/CheckStudents",
+        tech: ["devicon-python-plain"]
     },
     {
         name: "Second Portfolio",
         description: "This portfolio is a website made with ReactJS and tailwindcss. It also host some pictures I took during my trips.",
         picture: websiteIcon2,
         link: "https://bugbear.fr/",
+        tech: ["devicon-nodejs-plain", "devicon-react-original", "devicon-tailwindcss-plain"]
     },
     {
         name: 'First Portfolio',
         description: 'My first personal Portfolio, made with bootstrap, and few other libraries.',
         picture: websiteIcon,
-        link: 'https://me.bugbear.fr/'
+        link: 'https://me.bugbear.fr/',
+        tech: ["devicon-bootstrap-plain", "devicon-html5-plain", "devicon-css3-plain", "devicon-javascript-plain"]
     },
     {
-        name:"Other Projects",
-        description:"Here are some other projects I made during my studies.",
+        name: "Other Projects",
+        description: "Here are some other projects I made during my studies.",
         link: "https://github.com/Renaud-Dov?tab=repositories",
+        tech: []
     }
 ]
 
@@ -63,7 +69,7 @@ class ProjectItem extends Component {
         let text_color = "text-green-500";
         if (this.props.item.picture !== undefined) {
             bg = <img src={this.props.item.picture} alt="Avatar"
-                      className={"object-cover w-full h-full  opacity-50"}/>
+                      className={"object-cover w-full h-full  " + (this.state.isHovered ? "opacity-30" : "opacity-70")}/>
         } else {
             bg = <div className={"w-full h-full bg-green-500"}/>
             text_color = "text-white"
@@ -76,14 +82,24 @@ class ProjectItem extends Component {
                     className={"absolute object-fill w-full h-full inset-y-0 text-white font-bold text-xl text-left transition-all"}>
                     <div className="flex flex-wrap flex-row gap-10 p-5 ">
                         <div
-                            className={"absolute object-fill hover:underline hover:decoration-1 w-full h-full visible duration-200 inset-y-0 pl-4 pt-2 text-white font-medium text-2xl md:text-4xl text-left transition-all"}>
+                            className={(this.state.isHovered ? 'underline ' : ' ') +"absolute object-fill decoration-1 h-full visible duration-200 inset-y-0 pl-4 pt-2 text-white font-medium text-2xl md:text-4xl text-left transition-all"}>
                             {this.props.item.name}
                         </div>
                         <div className={(this.state.isHovered ? 'visible ' : 'invisible ') + "absolute pt-8 pl-4"}>
                             <p>{this.props.item.description}</p>
-                            <a href={this.props.item.link} className={text_color + " hover:underline  hover:decoration-2 pt-3"}>
+                            <a href={this.props.item.link}
+                               className={text_color + " hover:underline  hover:decoration-2 pt-3"}>
                                 Check this out <i className="fa-solid  fa-arrow-up-right-from-square px-1 md:fa-lg"/>
                             </a>
+                            <div className="flex flex-wrap  gap-4 pt-2 right-0">
+                                {this.props.item.tech.map((tech, index) => {
+                                    return (
+                                        <div key={index} className="flex flex-row items-center">
+                                            <i className={tech}/>
+                                        </div>
+                                    )
+                                })}
+                            </div>
                         </div>
                     </div>
                 </div>
