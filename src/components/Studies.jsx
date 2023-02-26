bb0bbb0d528886c534b3be732c4d8ce6f5b06772
Dov Devers <contact@bugbear.fr>
2023-02-26 18:05:18 +0100

    fixed studies pictures

--- src/components/Studies.jsx
+++ src/components/Studies.jsx
@@ -42,14 +42,14 @@ export function Studies() {
 
     return <>
         <h1 className="text-4xl md:text-5xl font-Lato text-left text-green-600 ml-3 md:ml-0 font-bold py-8">{data.studies}</h1>
-        <ul className="ml-5">
+        <ul className="ml-5 flex flex-col">
             {studies.map((study, index) =>
                 <li key={study.name} className="flex">
                     {/*picture*/}
                     <div
-                        className={(index === studies.length - 1 ? "" : "bg-dot-pattern") + " bg-repeat-y justify-center justify-items-start z-1 justify-self-center content-center place-content-center bg-center object-scale-down"}>
+                        className={(index === studies.length - 1 ? "" : "bg-dot-pattern") + " w-16 bg-repeat-y justify-center justify-items-start z-1 justify-self-center content-center place-content-center bg-center object-scale-down"}>
                         <img src={study.logo} alt={study.name}
-                             className="object-scale-down w-full w-16 h-16 mx-auto  rounded-full bg-white border-green-500 border-4"/>
+                             className="object-scale-down w-16 h-16 rounded-full bg-white border-green-500 border-4"/>
                     </div>
                     {/*description*/}
                     <div className={(index === studies.length - 1 ? "" : "mb-10 ") + " ml-5"}>
